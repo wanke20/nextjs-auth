6fe15e9a454489135c62e601779f2f078234f79c
wanke20 <122842952+wanke20@users.noreply.github.com>
2024-05-08 23:13:57 -0400

    first commit

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -15,6 +15,9 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import Link from 'next/link';
 import GoogleSignInButton from '../GoogleSignInButton';
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
+import { useToast } from '../ui/use-toast';
 
 const FormSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Invalid email'),
@@ -25,6 +28,8 @@ const FormSchema = z.object({
 });
 
 const SignInForm = () => {
+  const router = useRouter();
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -33,8 +38,23 @@ const SignInForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof FormSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    const signInData = await signIn('credentials', {
+      email: values.email,
+      password: values.password,
+      redirect: false,
+    });
+    
+    if (signInData?.error) {
+      toast({
+        title: "Error",
+        description: "Oops! Something went wrong!",
+        variant: 'destructive',
+      })
+    } else {
+      router.refresh();
+      router.push('/admin');
+    }
   };
 
   return (
---
+++ src/lib/auth.ts
@@ -0,0 +1,78 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "./db";
+import { compare } from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "john@mail.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const existingUser = await db.user.findUnique({
+          where: { email: credentials?.email },
+        });
+        if (!existingUser) {
+          return null;
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password
+        );
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: `${existingUser.id}`,
+          username: existingUser.username,
+          email: existingUser.email,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+            ...token,
+            username: user.username,
+        }
+      }
+      return token
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+            ...session.user,
+            username: token.username
+        }
+      }
+    },
+  }
+};
